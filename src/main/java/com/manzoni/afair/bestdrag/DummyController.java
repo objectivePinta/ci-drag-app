1845fca6998792df3aab03c164218f236ec6ff30
Andrei Pinta <andrei.pinta@metrosystems.net>
2017-10-26 21:34:02 +0300

    NO-TICKET - Change message

--- src/main/java/com/manzoni/afair/bestdrag/DummyController.java
+++ src/main/java/com/manzoni/afair/bestdrag/DummyController.java
@@ -3,11 +3,13 @@ package com.manzoni.afair.bestdrag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+
 @RestController("dummyController")
 public class DummyController {
 
     @GetMapping(path = "/drag")
     public String getTheDummy() {
-        return "Fah, martzafoaico!";
+        return "Martzafoaico, este ora " + Instant.now() + " Afara! Afara!";
     }
 }
